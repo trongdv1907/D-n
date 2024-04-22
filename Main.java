e0ffef533b4e14464ac957dce4b8555af08dc98e
CodeAppBeefood <trongdv1907@gmail.com>
2024-04-22 19:28:46 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,37 @@
+//Bài 1
+public class Main {
+    public static void main(String[] args) {
+        // Khai báo và gán giá trị cho biến soA và soB
+        int soA = 10;
+        int soB = 30;
+        
+        // Tính tổng của soA và soB
+        int tongHaiSo = soA + soB;
+        
+        // Hiển thị kết quả ra màn hình
+        System.out.println("soA = " + soA);
+        System.out.println("soB = " + soB);
+        System.out.println("Tong hai so = " + tongHaiSo);
+    }
+}
+
+// // Bài 2
+// public class Main {
+//     public static void main(String[] args) {
+        
+//         double a = 0; 
+//         double b = 0;
+        
+//         if (a == 0) {
+//             if (b == 0) {
+//                 System.out.println("PT co vo so nghiem");
+//             } else {
+//                 System.out.println("PT vo nghiem");
+//             }
+//         } else {
+//             double x = -b / a;
+//             System.out.println("Nghiem cua PT: x = " + x);
+//         }
+//     }
+// }
+
