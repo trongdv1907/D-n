23e6f78b0a25414a2a675da9f19eb489ea18cab0
CodeAppBeefood <trongdv1907@gmail.com>
2024-04-25 15:33:58 +0700

    main

---
+++ Student.java
@@ -0,0 +1,56 @@
+public class Student {
+    private int id;
+    private String name;
+    private int age;
+    private String address;
+    private double gpa;
+
+    public Student(int id, String name, int age, String address, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.gpa = gpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Age: " + age + ", Address: " + address + ", GPA: " + gpa;
+    }
+}
